fea60bb3ff05bc4e99b522f70c4331cb6eed04c1
Alex Lebeau <leba37@uqo.ca>
2020-02-10 19:47:22 -0500

    Client, documents et Main ajoute

---
+++ Client.vb
@@ -0,0 +1,17 @@
+Public Class Client
+    ReadOnly Property Nom As String
+    ReadOnly Property TelephoneNum As String
+    ReadOnly Property Courriel As String
+
+    Private Sub ReserverDoc()
+
+    End Sub
+
+    Private Sub UnreserveDoc()
+
+    End Sub
+
+    Private Sub Renouveller()
+
+    End Sub
+End Class
